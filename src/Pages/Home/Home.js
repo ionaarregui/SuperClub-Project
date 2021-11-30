a5823f70cc36e7ed5720e0d7cab00ca87682ccf1
Nikos90 <nikosanarg@gmail.com>
2021-11-30 18:48:31 -0300

    Incorporacion home antiguo

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,11 @@ import NewProduct from "../../Components/NewProduct/NewProduct"
 const Home = () => {
   return (
     <div>
-      <Home />
+      <h1>HOME PREPARA2 Y RUTEA2</h1>
+      <p>dasda ad sd as dsa dsa ads asd sd dsa ads</p>
+      <p>da dsa d w da d3 ad 2d 2d2a3cs 3f gr r3q f4 </p>
+      <p>few rg4 g 5h 65 7j6 k68 jh64g53 4f </p>
+      <NewProduct></NewProduct>
     </div>
   )
 }
