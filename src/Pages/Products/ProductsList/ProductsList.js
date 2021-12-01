653f656131ee5a2d37f9bd5e1754932b30752586
SolMansilla <mariasmansilla@santandertecnologia.com.ar>
2021-12-01 10:51:32 -0300

    codigo mas visual

--- src/Pages/Products/ProductsList/ProductsList.js
+++ src/Pages/Products/ProductsList/ProductsList.js
@@ -23,7 +23,16 @@ const ProductsList = () => {
       {loading ? <h3>loading...</h3> :
     <ul className="divMain">
       {products.map((prod, i, i2, i3, i4, i5, i6, i7) => {
-        return <li key={i} className="productos"><img key={i2} src={prod.image} alt="Foto producto" className="imagenesProductos"></img><div key={i3} className="listaProductos"> <h3 key={i4}>{prod.title}</h3> <h4 key={i5}>#{prod._id}</h4></div><Link key={i6} to={`/products/${prod._id}`} className="flechita"><img key={i7} src="flechita.svg" alt="Foto flechita"></img></Link></li>
+        return <li key={i} className="productos">
+          <img key={i2} src={prod.image} alt="Foto producto" className="imagenesProductos"></img>
+          <div key={i3} className="listaProductos"> 
+            <h3 key={i4}>{prod.title}</h3> 
+            <h4 key={i5}>#{prod._id}</h4>
+          </div>
+          <Link key={i6} to={`/products/${prod._id}`} className="flechita">
+            <img key={i7} src="flechita.svg" alt="Foto flechita"></img>
+          </Link>
+        </li>
       })}
     </ul>}
     </>
--- src/Pages/Stores/StoresList/StoresList.js
+++ src/Pages/Stores/StoresList/StoresList.js
@@ -23,7 +23,16 @@ const StoresList = () => {
       {loading ? <h3>loading...</h3> :
     <ul className="divMain">
       {stores.map((store, i, i2, i3, i4, i5, i6, i7) => {
-        return <li key={i} className="tiendas"><img key={i2} src={store.logo} alt="Foto tienda" className="imagenesTiendas"></img><div key={i3} className="listaStores"> <h3 key={i4}>{store.name}</h3> <h4 key={i5}>#{store._id}</h4></div><Link key={i6} to={`/stores/${store._id}`} className="flechita"><img key={i7} src="flechita.svg" alt="Foto flechita"></img></Link></li>
+        return <li key={i} className="tiendas">
+          <img key={i2} src={store.logo} alt="Foto tienda" className="imagenesTiendas"></img>
+          <div key={i3} className="listaStores"> 
+            <h3 key={i4}>{store.name}</h3> 
+            <h4 key={i5}>#{store._id}</h4>
+          </div>
+          <Link key={i6} to={`/stores/${store._id}`} className="flechita">
+            <img key={i7} src="flechita.svg" alt="Foto flechita"></img>
+          </Link>
+      </li>
       })}
     </ul>}
     </>
