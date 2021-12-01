c47014b223cfcc22bdd2b5fc2018600a65e7c05c
Kajachuan <kevincajachuan@hotmail.com>
2021-12-01 14:27:37 -0300

    Borro setvalue

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -20,7 +20,7 @@ const Header = ({ handler, searchContext }) => {
     content = (
       <>
         <div>
-          <div onClick={() => setValue(!value)}>
+          <div>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               xmlnsXlink="http://www.w3.org/1999/xlink"
